6f416a2cb48177458ab6f76031717d21a6d34ca7
deesh1211 <deesh1211@users.noreply.github.com>
2015-03-21 16:49:43 +0530

    dasdas

--- src/test_test/sample.java
+++ src/test_test/sample.java
@@ -1,19 +1,18 @@
 package test_test;
 
 public class sample {
-	public static void main (String args[]){
-		
+	public static void main(String args[]) {
+
 		System.out.println("This is a test");
 		System.out.println("This is a test2");
 		System.out.println("This is a test3");
 		System.out.println("This is a test4");
 		System.out.println("This is a test5");
-		
+
 		for (int i = 0; i < 10; i++) {
 			System.out.println(i);
 		}
-		
-		
+
 	}
 
 }
